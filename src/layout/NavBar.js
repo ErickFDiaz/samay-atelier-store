45f6bd2d9454a8d2a6a37bf87a6636b25a75bbb4
efdiaz <efdiaz.dev@gmail.com>
2022-07-18 21:43:21 -0500

    Navbar added

---
+++ src/layout/NavBar.js
@@ -0,0 +1,35 @@
+// import { Fragment } from "react";
+
+const NavBar = () => {
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container-fluid">
+                    <a className="navbar-brand fs-3" href="#">Samay Atelier</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarColor01">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Inicio</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Tienda</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Contacto</a>
+                            </li>
+                        </ul>
+                        <form className="d-flex" role="search">
+                            <input className="form-control me-2" type="search" placeholder="Buscar" aria-label="Search"/>
+                                <button className="btn btn-outline-light" type="submit">Buscar</button>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default NavBar;
