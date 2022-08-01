009743fc72b5576cdfbefeb4dce9de02a5f5d452
efdiaz <efdiaz.dev@gmail.com>
2022-08-01 00:59:09 -0500

    Some styles  improved !

--- src/components/Item.js
+++ src/components/Item.js
@@ -5,10 +5,11 @@ const Item = ({product}) => {
 
     return (
         <>
-            <div className="card" style={{width: '18rem', height: '510px'}}>
+            <div className="card" style={{width: '18rem', height: '530px'}}>
                 <img src={pictureUrl} className="card-img-top" alt="..."/>
                     <div className="card-body">
-                        <h5 className="card-title">{title} -  <span className='fw-bold'>S/. {price}</span></h5>
+                        <h5 className="card-title" style={{height:'50px'}}>{title}</h5>
+                        <h5 className='fw-bold'>S/.{price}</h5>
                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         <a href="#" className="btn btn-primary">Go somewhere</a>
                     </div>
